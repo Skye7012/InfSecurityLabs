123732f18f783a06bf99b90f0f72365bc462374c
Skye7012 <skye7012@gmail.com>
2021-11-06 00:43:14 +0300

    PowLab Mvp

---
+++ Labs/PowLab/PowForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Labs.PowLab
+{
+	partial class PowForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.mainComboBox = new System.Windows.Forms.ToolStripComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.varGroupBox = new System.Windows.Forms.GroupBox();
+			this.calculateBtn = new System.Windows.Forms.Button();
+			this.nTbx = new System.Windows.Forms.TextBox();
+			this.nLbl = new System.Windows.Forms.Label();
+			this.bTbx = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.aTbx = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.zTbx = new System.Windows.Forms.TextBox();
+			this.zLbl = new System.Windows.Forms.Label();
+			this.menuStrip1.SuspendLayout();
+			this.varGroupBox.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainComboBox});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// mainComboBox
+			// 
+			this.mainComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.mainComboBox.Name = "mainComboBox";
+			this.mainComboBox.Size = new System.Drawing.Size(121, 29);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(303, 75);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(113, 21);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "a^b mod n = z";
+			// 
+			// varGroupBox
+			// 
+			this.varGroupBox.Controls.Add(this.calculateBtn);
+			this.varGroupBox.Controls.Add(this.nTbx);
+			this.varGroupBox.Controls.Add(this.nLbl);
+			this.varGroupBox.Controls.Add(this.bTbx);
+			this.varGroupBox.Controls.Add(this.label3);
+			this.varGroupBox.Controls.Add(this.aTbx);
+			this.varGroupBox.Controls.Add(this.label2);
+			this.varGroupBox.Location = new System.Drawing.Point(12, 113);
+			this.varGroupBox.Name = "varGroupBox";
+			this.varGroupBox.Size = new System.Drawing.Size(776, 211);
+			this.varGroupBox.TabIndex = 3;
+			this.varGroupBox.TabStop = false;
+			// 
+			// calculateBtn
+			// 
+			this.calculateBtn.Dock = System.Windows.Forms.DockStyle.Top;
+			this.calculateBtn.Enabled = false;
+			this.calculateBtn.Location = new System.Drawing.Point(3, 175);
+			this.calculateBtn.Name = "calculateBtn";
+			this.calculateBtn.Size = new System.Drawing.Size(770, 30);
+			this.calculateBtn.TabIndex = 6;
+			this.calculateBtn.Text = "Calculate";
+			this.calculateBtn.UseVisualStyleBackColor = true;
+			this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+			// 
+			// nTbx
+			// 
+			this.nTbx.Dock = System.Windows.Forms.DockStyle.Top;
+			this.nTbx.Location = new System.Drawing.Point(3, 146);
+			this.nTbx.Name = "nTbx";
+			this.nTbx.Size = new System.Drawing.Size(770, 29);
+			this.nTbx.TabIndex = 5;
+			// 
+			// nLbl
+			// 
+			this.nLbl.Dock = System.Windows.Forms.DockStyle.Top;
+			this.nLbl.Location = new System.Drawing.Point(3, 125);
+			this.nLbl.Name = "nLbl";
+			this.nLbl.Size = new System.Drawing.Size(770, 21);
+			this.nLbl.TabIndex = 4;
+			this.nLbl.Text = "n";
+			// 
+			// bTbx
+			// 
+			this.bTbx.Dock = System.Windows.Forms.DockStyle.Top;
+			this.bTbx.Location = new System.Drawing.Point(3, 96);
+			this.bTbx.Name = "bTbx";
+			this.bTbx.Size = new System.Drawing.Size(770, 29);
+			this.bTbx.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label3.Location = new System.Drawing.Point(3, 75);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(770, 21);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "b";
+			// 
+			// aTbx
+			// 
+			this.aTbx.Dock = System.Windows.Forms.DockStyle.Top;
+			this.aTbx.Location = new System.Drawing.Point(3, 46);
+			this.aTbx.Name = "aTbx";
+			this.aTbx.Size = new System.Drawing.Size(770, 29);
+			this.aTbx.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.label2.Location = new System.Drawing.Point(3, 25);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(770, 21);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "a";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.zTbx);
+			this.groupBox2.Controls.Add(this.zLbl);
+			this.groupBox2.Location = new System.Drawing.Point(15, 324);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(770, 114);
+			this.groupBox2.TabIndex = 4;
+			this.groupBox2.TabStop = false;
+			// 
+			// zTbx
+			// 
+			this.zTbx.Dock = System.Windows.Forms.DockStyle.Top;
+			this.zTbx.Location = new System.Drawing.Point(3, 46);
+			this.zTbx.Name = "zTbx";
+			this.zTbx.ReadOnly = true;
+			this.zTbx.Size = new System.Drawing.Size(764, 29);
+			this.zTbx.TabIndex = 10;
+			// 
+			// zLbl
+			// 
+			this.zLbl.Dock = System.Windows.Forms.DockStyle.Top;
+			this.zLbl.Location = new System.Drawing.Point(3, 25);
+			this.zLbl.Name = "zLbl";
+			this.zLbl.Size = new System.Drawing.Size(764, 21);
+			this.zLbl.TabIndex = 9;
+			this.zLbl.Text = "z";
+			// 
+			// PowForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.varGroupBox);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.menuStrip1);
+			this.Name = "PowForm";
+			this.Text = "PowForm";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.varGroupBox.ResumeLayout(false);
+			this.varGroupBox.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripComboBox mainComboBox;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox varGroupBox;
+		private System.Windows.Forms.TextBox aTbx;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox bTbx;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button calculateBtn;
+		private System.Windows.Forms.TextBox nTbx;
+		private System.Windows.Forms.Label nLbl;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.TextBox zTbx;
+		private System.Windows.Forms.Label zLbl;
+	}
+}
---
+++ Labs/PowLab/PowForm.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Numerics;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Labs.PowLab
+{
+	public partial class PowForm : Form
+	{
+		BigInteger _a;
+		BigInteger _b;
+		BigInteger _n;
+		List<TextBox> _varTextBoxes;
+		List<BigInteger> _vars;
+		public PowForm()
+		{
+			InitializeComponent();
+			_varTextBoxes = new List<TextBox>()
+			{
+				aTbx, bTbx,nTbx,
+			};
+
+			_vars = new List<BigInteger>()
+			{
+				_a, _b, _n
+			};
+
+			foreach (var item in _varTextBoxes)
+			{
+				item.TextChanged += new EventHandler(VariablesTextChanged);
+			}
+		}
+
+		#region Logic
+
+		BigInteger Calculate()
+		{
+			MapVars();
+
+			var binDegree = _b.ToBinaryString().TrimStart('0');
+			var aS = new List<BigInteger>() { _a };
+
+			for (int i = 1; i < binDegree.Length; i++)
+			{
+				int b = Convert.ToInt32(binDegree[i].ToString());
+
+				if (b == 0)
+					aS.Add((aS[i - 1] * aS[i - 1]) % _n);
+				else if (b == 1)
+					aS.Add((aS[i - 1] * aS[i - 1] * _a) % _n);
+			}
+
+			return aS.Last();
+		}
+
+		void MapVars()
+		{
+			_a = BigInteger.Parse(aTbx.Text);
+			_b = BigInteger.Parse(bTbx.Text);
+			_n = BigInteger.Parse(nTbx.Text);
+		}
+
+		#endregion
+
+
+		#region Servise
+
+		bool AreVariablesValid()
+		{
+			BigInteger temp;
+
+			bool isValid = _varTextBoxes.All(x => BigInteger.TryParse
+				 (x.Text, out temp)
+				 && !string.IsNullOrWhiteSpace(x.Text));
+
+			if (isValid)
+				return true;
+			return false;
+		}
+
+		private void VariablesTextChanged(object sender, EventArgs e)
+		{
+			if (AreVariablesValid())
+				calculateBtn.Enabled = true;
+			else
+				calculateBtn.Enabled = false;
+		}
+
+		private void calculateBtn_Click(object sender, EventArgs e)
+		{
+			zTbx.Text = Convert.ToString(Calculate());
+		}
+
+		#endregion
+	}
+}
